2af6849a98cb0b304783bb5012319d0de029a925
Jack <shiungchenyu@gmail.com>
2019-01-15 16:19:12 +0800

    主機api 網址變動

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: '"production"',
   // API_HOST: '"https://hs_api.neplus.com.tw/api/"'
   API_HOST: '"https://t.toseek.info/api/"' // 測試機
-  // API_HOST: '"https://api.hsvibe.com/"' // 主機
+  // API_HOST: '"https://api.hsvibe.com/api/"' // 主機
 }
